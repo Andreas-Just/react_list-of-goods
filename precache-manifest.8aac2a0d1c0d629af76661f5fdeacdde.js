dddf883f19693458fcfe67c77baaeda7bb81c397
Andreas_Just <andreas.wq@gmail.com>
2020-04-30 17:28:47 +0300

    Updates

---
+++ precache-manifest.8aac2a0d1c0d629af76661f5fdeacdde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445883fc550c1e6e0ab0b07445b92eaf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aefe23d57b9146f19f7f",
+    "url": "./static/css/main.9ac12977.chunk.css"
+  },
+  {
+    "revision": "9de26736ef01b9e7c5a0",
+    "url": "./static/js/2.e55650f8.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "./static/js/2.e55650f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefe23d57b9146f19f7f",
+    "url": "./static/js/main.98de2e60.chunk.js"
+  },
+  {
+    "revision": "5094eb25b1a0c08273e9",
+    "url": "./static/js/runtime-main.3cab93c1.js"
+  },
+  {
+    "revision": "1d5025de89768964e8e43713c947f7b8",
+    "url": "./static/media/horiz.1d5025de.png"
+  }
+]);
